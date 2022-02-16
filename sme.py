b48b7872fb376a1bbd95c6ed98cb9016a5334106
Goodness Ojeaga <ojeaga47@gmail.com>
2022-02-16 19:40:16 +0100

    imported sme.csv with pandas

---
+++ sme.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import time
+import pandas as pd
+#mport matplotlib.pyplot as plt
+
+
+st.title('SMART LIGHT')
+st.write("An SME that aims to illuminate the planet with solar panels!")
+with st.expander("Read More..."):
+                 st.write('A SME Company called SMART LIGHT specialized in selling panels possesses 5 subsidiaries across two continent namely in the cities New York, Los Angeles, London, Paris and Munich. These cities are internally referenced by thier codes C001,....C005, respectively. At the end of each quater, a subsidiary should communicate to the headquater the ammounts of sales and costs done during the quater, however, monthly detailed. Based on the communicated data, the head quaters wants to produce a quaterly that should entail statistics related to sales and profits done either individually or globally. These statistics should be helpful for eveluating the activities of each subsidiary and in making business decisions by the direction.')
+
+sme_sales=pd.read_csv('sme.csv')
+
+
+data_sme= pd.DataFrame(sme_sales)
+st.table(data_sme)
