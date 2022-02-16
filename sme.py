87f74bf24f27d16c848114678c391ace3b3e9232
Goodness Ojeaga <ojeaga47@gmail.com>
2022-02-16 19:54:55 +0100

    added an image

--- sme.py
+++ sme.py
@@ -2,10 +2,17 @@ import streamlit as st
 import time
 import pandas as pd
 #mport matplotlib.pyplot as plt
+from PIL import Image
+
+
 
 
 st.title('SMART LIGHT')
+
 st.write("An SME that aims to illuminate the planet with solar panels!")
+image = Image.open('sme_panels.jpg')
+st.image(image, use_column_width="auto")
+
 with st.expander("Read More..."):
                  st.write('A SME Company called SMART LIGHT specialized in selling panels possesses 5 subsidiaries across two continent namely in the cities New York, Los Angeles, London, Paris and Munich. These cities are internally referenced by thier codes C001,....C005, respectively. At the end of each quater, a subsidiary should communicate to the headquater the ammounts of sales and costs done during the quater, however, monthly detailed. Based on the communicated data, the head quaters wants to produce a quaterly that should entail statistics related to sales and profits done either individually or globally. These statistics should be helpful for eveluating the activities of each subsidiary and in making business decisions by the direction.')
 
